1a08d309e94209f4ff8d0841ecb6bbcb928ff368
João Manoel Sobral da Costa <joaomsobral.drovak@gmail.com>
2025-09-08 16:40:00 -0300

    Updated stats.py: Added function to return dict cointaining the count of each alphabetic character. Updated main.py: Added error handling and updated the format of the result that is printed on console.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 from stats import get_word_count, get_character_count
 
@@ -6,13 +7,25 @@ def get_book_text(file_path):
         return file.read()
     
 def main():
-    book = ("books/frankenstein.txt")
-    book_text = get_book_text(book)
+    if len(sys.argv) != 2:
+        print("Wrong argument format! Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    try:
+        book_text = get_book_text(book_path)
+    except FileNotFoundError as e:
+        print(e)
+        sys.exit(2)
     word_count = get_word_count(book_text)
     character_count_dict = get_character_count(book_text)
-    print(f"{word_count} words found in the document")
-    for key, value in character_count_dict.items():
-        print(f"\'{key}\': {value}")
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for key, value in character_count_dict.items():
+        print(f"{key}: {value}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,12 +1,27 @@
 def get_word_count(book_text: str):
     return len(book_text.split())
 
+def sort_on(items):
+    return items["num"]
+
 def get_character_count(book_text: str):
     normalized_lower_case_text = book_text.lower()
     character_count_dict = {}
     for letter in normalized_lower_case_text:
         if letter not in character_count_dict:
-            character_count_dict[letter] = 1
+            if letter.isalpha():
+                character_count_dict[letter] = 1
         else:
             character_count_dict[letter] = character_count_dict[letter] + 1
+    
+    dict_list_to_sort = []
+    temp_dict = {}
+    for key, value in character_count_dict.items():
+        temp_dict = {"name": key, "num": value}
+        dict_list_to_sort.append(temp_dict)
+    dict_list_to_sort.sort(reverse=True, key=sort_on)
+    temp_dict = {}
+    for dict_item in dict_list_to_sort:
+        temp_dict[dict_item["name"]] = dict_item["num"]
+    character_count_dict = temp_dict
     return character_count_dict
